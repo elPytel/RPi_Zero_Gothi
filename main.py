766d41cb244901adfebd55c914e7d90d34e00a5d
elPytel <jaroslav.korner1@gmail.com>
2025-05-02 10:40:13 +0200

    status bar charging

--- main.py
+++ main.py
@@ -87,71 +87,102 @@ if __name__=='__main__':
     disp.ShowImage(disp.getbuffer(image))
     time.sleep(1)
 
-    while True:
-        bus_voltage = ina219.getBusVoltage_V()             # voltage on V- (load side)
-        shunt_voltage = ina219.getShuntVoltage_mV() / 1000 # voltage between V+ and V- across the shunt
-        current = ina219.getCurrent_mA()                   # current in mA
-        power = ina219.getPower_W()                        # power in W
-        percent = ina219.getRemainingPercent()
-        remaining_time = ina219.getRemainingTime()
+    class Timer:
+        def __init__(self, sleep_time=1):
+            self.start_time = time.time()
+            self.sleep_time = sleep_time
+            self.sleep_until = self.start_time + sleep_time
 
-        # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
-        if DEBUG:
-            print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
-            print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
-            print("Load Voltage:  {:6.3f} V".format(bus_voltage))
-            print("Current:       {:6.3f} A".format(current/1000))
-            print("Power:         {:6.3f} W".format(power))
-            print("Percent:       {:3.1f}%".format(percent))
-            print("")
+        def elapsed(self):
+            return time.time() - self.start_time
         
-        if current > 0:
-            # charging
-            print_info("Charging")
+        def reset(self):
+            self.start_time = time.time()
+            self.sleep_until = self.start_time + self.sleep_time
 
-        texts = []
-        #text = f"Voltage: {bus_voltage:6.3f} V"
-        text = f"Time:    {sec_to_hhmmss(remaining_time)}"
-        texts.append(text)
-        text = f"Current: {current/1000:6.3f} A"
-        texts.append(text)
-        text = f"Power:   {power:6.3f} W"
-        texts.append(text)
-        text = f"Percent:  {percent:3.1f}%"
-        texts.append(text)
+        def sleep(self, seconds):
+            self.sleep_until = time.time() + seconds
+            self.sleep_time = seconds
+        
+        def done(self):
+            if time.time() >= self.sleep_until:
+                self.reset()
+                return True
+            return False
+    
+    
+    sleep_1s = Timer(1)
+    sleep_100ms = Timer(0.01)
 
-        # clear the image
-        image = Image.new('1', (disp.width, disp.height), "WHITE")
-        draw = ImageDraw.Draw(image)
-        x_pos = 5
-        y_pos = 2
-        x_shift = 0
-        y_shift = 14
-        for text in texts:
-            draw.text((x_pos, y_pos), text, font = font10, fill = 0)
-            x_pos += x_shift
-            y_pos += y_shift
-        disp.ShowImage(disp.getbuffer(image))
+    while True:
+        if sleep_1s.done():
+            bus_voltage = ina219.getBusVoltage_V()             # voltage on V- (load side)
+            shunt_voltage = ina219.getShuntVoltage_mV() / 1000 # voltage between V+ and V- across the shunt
+            current = ina219.getCurrent_mA()                   # current in mA
+            power = ina219.getPower_W()                        # power in W
+            percent = ina219.getRemainingPercent()
+            remaining_time = ina219.getRemainingTime()
 
-        # Kontrola stisknutí kláves
-        if disp.RPI.digital_read(disp.RPI.GPIO_KEY_UP_PIN):
-            print("Key UP pressed")
-        if disp.RPI.digital_read(disp.RPI.GPIO_KEY_DOWN_PIN):
-            print("Key DOWN pressed")
-        if disp.RPI.digital_read(disp.RPI.GPIO_KEY_LEFT_PIN):
-            print("Key LEFT pressed")
-        if disp.RPI.digital_read(disp.RPI.GPIO_KEY_RIGHT_PIN):
-            print("Key RIGHT pressed")
-        if disp.RPI.digital_read(disp.RPI.GPIO_KEY_PRESS_PIN):
-            print("Key ENTER pressed")
-        if disp.RPI.digital_read(disp.RPI.GPIO_KEY1_PIN):
-            print("Key 1 pressed")
-        if disp.RPI.digital_read(disp.RPI.GPIO_KEY2_PIN):
-            print("Key 2 pressed")
-        if disp.RPI.digital_read(disp.RPI.GPIO_KEY3_PIN):
-            print("Key 3 pressed")
+            # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
+            if DEBUG:
+                print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
+                print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
+                print("Load Voltage:  {:6.3f} V".format(bus_voltage))
+                print("Current:       {:6.3f} A".format(current/1000))
+                print("Power:         {:6.3f} W".format(power))
+                print("Percent:       {:3.1f}%".format(percent))
+                print("")
+            
+            if current > 0:
+                # charging
+                print_info("Charging")
 
-        time.sleep(2)
+            texts = []
+            #text = f"Voltage: {bus_voltage:6.3f} V"
+            text = f"Time:    {sec_to_hhmmss(remaining_time)}"
+            texts.append(text)
+            text = f"Current: {current/1000:6.3f} A"
+            texts.append(text)
+            text = f"Power:   {power:6.3f} W"
+            texts.append(text)
+            text = f"Percent:  {percent:3.1f}%"
+            texts.append(text)
+
+            # clear the image
+            image = Image.new('1', (disp.width, disp.height), "WHITE")
+            draw = ImageDraw.Draw(image)
+            x_pos = 5
+            y_pos = 2
+            x_shift = 0
+            y_shift = 14
+            for text in texts:
+                draw.text((x_pos, y_pos), text, font = font10, fill = 0)
+                x_pos += x_shift
+                y_pos += y_shift
+            disp.ShowImage(disp.getbuffer(image))
+
+        if sleep_100ms.done():
+            # Kontrola stisknutí kláves
+            if not RPI:
+                disp.window.update_idletasks()
+            if disp.RPI.digital_read(disp.RPI.GPIO_KEY_UP_PIN):
+                print("Key UP pressed")
+            if disp.RPI.digital_read(disp.RPI.GPIO_KEY_DOWN_PIN):
+                print("Key DOWN pressed")
+            if disp.RPI.digital_read(disp.RPI.GPIO_KEY_LEFT_PIN):
+                print("Key LEFT pressed")
+            if disp.RPI.digital_read(disp.RPI.GPIO_KEY_RIGHT_PIN):
+                print("Key RIGHT pressed")
+            if disp.RPI.digital_read(disp.RPI.GPIO_KEY_PRESS_PIN):
+                print("Key ENTER pressed")
+            if disp.RPI.digital_read(disp.RPI.GPIO_KEY1_PIN):
+                print("Key 1 pressed")
+            if disp.RPI.digital_read(disp.RPI.GPIO_KEY2_PIN):
+                print("Key 2 pressed")
+            if disp.RPI.digital_read(disp.RPI.GPIO_KEY3_PIN):
+                print("Key 3 pressed")
+
+        time.sleep(0.001)
     
     """
     draw.line([(0,0),(127,0)], fill = 0)
